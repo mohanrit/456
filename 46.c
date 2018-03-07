79fa4322481aa6a587a3738a18694d0ad5778292
mohanrit <35220535+mohanrit@users.noreply.github.com>
2018-03-07 16:58:24 +0530

    Update 46.c

--- 46.c
+++ 46.c
@@ -1,9 +1,11 @@
     #include<stdio.h>
-    void main()
+    int main()
     {
     	int a=5,n,s;
     	int b=4,c=5,d=6,e=7,f=8;
     	n=b+c+d+e+f;
     	s=n/5;
+        
+        
     	printf("%d",s);
     }
